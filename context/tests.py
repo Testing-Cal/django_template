0def57be339a1c3713139cc692556a0fbec8bb06
Abhijeet Kamble <akamble@calibo.com>
2024-08-02 16:33:23 +0530

    changes related to python test and coverage report

--- context/tests.py
+++ context/tests.py
@@ -1,8 +1,15 @@
-from django.test import TestCase
+# from django.test import TestCase
+#
+# # Create your tests here.
+#
+# class ContextTestCase(TestCase):
+#     def test_app(self):
+#         self.assertTrue(True)
+import pytest
 
 # Create your tests here.
 
-class ContextTestCase(TestCase):
-    def test_app(self):
-        self.assertTrue(True)
+@pytest.mark.django_db
+def test_app():
+    assert True
 
